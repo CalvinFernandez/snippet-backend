219d8330538e4187f4abc2569f0b74bf8f4abb6a
Eli Hart <konakid@gmail.com>
2013-09-09 12:59:45 -1000

    Create category class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130908231728) do
+ActiveRecord::Schema.define(version: 20130909014927) do
+
+  create_table "messages", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "contact_id"
+    t.integer  "song_id"
+    t.boolean  "sent"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "songs", force: true do |t|
+    t.string   "title"
+    t.string   "artist"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
