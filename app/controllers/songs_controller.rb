e6224aca871017abb0271b8dd31619ba405c24e8
Eli Hart <konakid@gmail.com>
2013-09-13 10:19:15 -1000

    Add url to songs endpoint

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,12 @@
 class SongsController < ApplicationController
   # List all songs
   def all
-    render json: Song.all
+    render json: Song.all.to_json(only: [:id, :title, :artist], methods: :path, include: {category: {only: [:id, :name]}})
   end
 
   # Show info for a specific song
   def show
-    render json: Song.find(params[:id])
+    song = Song.find(params[:id])
+    render json: song.to_json(only: [:id, :title, :artist], methods: :path, include: {category: {only: [:id, :name]}})
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :category
 
+  def path
+    title = self.title.downcase.gsub(' ', '_')
+    artist = self.artist.downcase.gsub(' ', '_')
+    category = self.category.name.downcase.gsub(' ', '_')
 
+    file_name = "#{title}##{artist}##{category}.mp3"
+
+    (Rails.root + "/songs/#{file_name}").to_s
+  end
 end
