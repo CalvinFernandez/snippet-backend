51b27157c0670d17a78ddd5cc84375f31ca54f01
Eli Hart <konakid@gmail.com>
2013-09-14 10:00:21 -1000

    Song data

--- db/migrate/20130910030936_test_data.rb
+++ db/migrate/20130910030936_test_data.rb
@@ -2,11 +2,11 @@ class TestData < ActiveRecord::Migration
   def change
     cat1 = Category.create(:name => 'Greetings')
     cat2 = Category.create(:name => 'Christmas')
-    cat3 = Category.create(:name => 'Good Morning')
+    cat3 = Category.create(:name => 'Love')
 
-    Song.create(:title => 'How are you today?', :artist => 'The Yummy Yummies', :category => cat1)
-    Song.create(:title => 'Mele Kalikimaka', :artist => 'Bing Crosby', :category => cat2)
-    Song.create(:title => 'Here comes the sun', :artist => 'The Beatles', :category => cat3)
+    Song.create(:title => 'Good Morning Good Morning', :artist => 'The Beatles', :category => cat1)
+    Song.create(:title => 'Two Of Us', :artist => 'The Beatles', :category => cat3)
+    Song.create(:title => 'Wonderful Tonight', :artist => 'Eric Clapton', :category => cat3)
 
     user1 = User.create(:email => 'test@example.com', :password => 'password', gender: 'male', first_name: 'eli', last_name: 'hart', age: 22)
 
