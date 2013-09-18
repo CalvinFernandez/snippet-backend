a32dfade296f8efafe1e0fff45cde2082a8953f0
Eli Hart <konakid@gmail.com>
2013-09-18 13:58:41 -1000

    Launch email

--- db/migrate/20130918203643_launch.rb
+++ db/migrate/20130918203643_launch.rb
@@ -2,7 +2,7 @@ class Launch < ActiveRecord::Migration
   def change
 
     password = Devise.friendly_token.first(8)
-    user1 = User.create!(email: 'cfernand@stanford.edu', password: password, gender: 'male',
+    user1 = User.create!(email: 'elihart@stanford.edu', password: password, gender: 'male',
                         first_name: 'calvin', last_name: 'fernandez', age: 22)
     UserMailer.launch(user1, password).deliver
   end
