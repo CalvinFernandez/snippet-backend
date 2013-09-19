ac860cec32e63c2280296d25757ff1b8574bb092
Eli Hart <konakid@gmail.com>
2013-09-19 09:12:24 -1000

    Update song names

--- db/migrate/20130918203643_launch.rb
+++ db/migrate/20130918203643_launch.rb
@@ -1,9 +1,9 @@
 class Launch < ActiveRecord::Migration
   def change
-    #
-    #password = Devise.friendly_token.first(8)
-    #user1 = User.create!(email: 'elihart@stanford.edu', password: password, gender: 'male',
-    #                    first_name: 'calvin', last_name: 'fernandez', age: 22)
-    #UserMailer.launch(user1, password).deliver
+
+    password = Devise.friendly_token.first(8)
+    user1 = User.create!(email: 'meredith@iteratti.com', password: password, gender: 'male',
+                        first_name: 'calvin', last_name: 'fernandez', age: 22)
+    UserMailer.launch(user1, password).deliver
   end
 end
