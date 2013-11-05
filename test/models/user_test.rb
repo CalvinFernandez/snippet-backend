2caaae08451b01ee18ffc6f09c57ea38fca8910c
Maqaw Team <maqaw.team@gmail.com>
2013-11-05 01:12:18 +0000

    user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,35 @@
 require 'test_helper'
 
+
 class UserTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
 
   test "create user" do
-    User
+    usr = User.new(:email => "test@test.com", :username => "helloworld", :password => "password")
+    require 'pry'
+    binding.pry
+    assert usr.save, "Should be able to save"
+  end
+
+  test "shouldn't create without email" do
+    usr = User.new(:username => "herro")
+    assert !usr.save, "shoulnd't be able to save without email"
+  end
+  
+  test "should'nt create without email" do 
+    usr = User.new(:email => 't@t.com')
+    assert !usr.save
+  end
+ 
+  test "email should be unique" do
+    usr = User.new(:email => 'test@test.com', :username => 'h')
+    assert !usr.save   
+  end
+
+  test "username should be unique" do
+    usr = User.new(:email => 'a@a.com', :username => 'helloworld')
+    assert !usr.save
   end
 end
