722f4b8681d869940561791e086d030a86b598b5
Maqaw Team <maqaw.team@gmail.com>
2013-11-08 00:10:16 +0000

    removed auth from friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -10,30 +10,35 @@ class FriendsController < ApplicationController
   end
   
   def create
-    username = params[:username]
-    email = params[:email]
-    user_id = params[:user_id]
-    new_friend = {}
+    _current_user = User.find_by_id(params[:user_id])
+    if _current_user.nil?
+      render :json => {}, :status => 400      
+    else
+      username = params[:username]
+      email = params[:email]
+      id = params[:id]
+      new_friend = {}
 
-    if username
-      new_friend = User.find_by_username(username)     
-    elsif email
-      new_friend = User.find_by_email(email)
-    elsif user_id
-      new_friend = User.find_by_id(user_id)
-    end 
+      if username
+        new_friend = User.find_by_username(username)     
+      elsif email
+        new_friend = User.find_by_email(email)
+      elsif id
+        new_friend = User.find_by_id(id)
+      end 
 
-    if new_friend
-       if current_user.friends.find_by_id(new_friend.id).nil?
-        # not already been friended. Add as friend
-        current_user.friends << new_friend 
-        render :json => current_user.friends, :status => 201 
+      if new_friend
+        if _current_user.friends.find_by_id(new_friend.id).nil?
+          # not already been friended. Add as friend
+          _current_user.friends << new_friend 
+          render :json => _current_user.friends, :status => 201 
+        else 
+          # already been friended. don't add again
+          render :json => _current_user.friends, :status => 400
+        end
       else 
-        # already been friended. don't add again
-        render :json => current_user.friends, :status => 400
+        render :json => {}, :status => 400
       end
-    else 
-      render :json => {}, :status => 400
     end
   end
 
@@ -41,13 +46,19 @@ class FriendsController < ApplicationController
   end
 
   def destroy
-    friend_id = params[:id]
-    friend = current_user.friends.find(friend_id) 
-    if friend
-      current_user.friends.destroy(friend)
-      render :json => {}, :status => 200
-    else 
+    _current_user = User.find_by_id(params[:user_id])
+    if _current_user.nil?
       render :json => {}, :status => 400
+    else
+      friend_id = params[:id]
+      friend = _current_user.friends.find(friend_id) 
+      if friend
+        _current_user.friends.destroy(friend)
+        render :json => {}, :status => 200
+      else 
+        render :json => {}, :status => 400
+      end
     end
   end
+
 end
